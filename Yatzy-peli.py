a72cb5eb344d21fdeaa0932b4335d05363f5cbea
unknown <karitakors@gmail.com>
2025-09-02 21:54:15 +0300

    vuoro ja noppa yhdistetty

--- Yatzy-peli.py
+++ Yatzy-peli.py
@@ -20,3 +20,18 @@ def pidettavat_nopat(nopat):
             return valitut
         except (ValueError, IndexError):
             print("Yritä uudelleen")
+
+def vuoro():
+    nopat = heita(5)
+    for heitto in range(2):
+        nayta_nopat(nopat)
+        valitut = pidettavat_nopat(nopat)
+        if len(valitut) == 5:
+            break
+        uudet = heita(5 - len(valitut))
+        nopat = valitut + uudet
+
+    return nopat
+
+
+vuoro()
